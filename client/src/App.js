5dbce246fe858dd5d3664b90d89954c0c9e2dce5
nngmi <arewenngmi@gmail.com>
2023-09-26 19:58:11 -0500

    basic tic tac toe game

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,91 @@
-import React from "react";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [data, setData] = React.useState(null);
+    const [games, setGames] = useState({});
+    const [selectedGame, setSelectedGame] = useState(null);
+    const [board, setBoard] = useState(Array(3).fill(Array(3).fill(null)));
+    const [currentPlayer, setCurrentPlayer] = useState('X');
+    const [gameState, setGameState] = useState('ongoing');
 
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
+    const createGame = async () => {
+        try {
+            const res = await axios.post('/api/game');
+            const gameId = res.data.gameId;
+            setGames((prev) => ({ ...prev, [gameId]: { board, currentPlayer, gameState } }));
+            setSelectedGame(gameId);
+        } catch (error) {
+            console.error("Error creating game", error);
+        }
+    };
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="/logo512.png" className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
-    </div>
-  );
+    const makeMove = async (row, col) => {
+        if (!selectedGame || board[row][col] || gameState !== 'ongoing') return;
+        try {
+            const res = await axios.post(`/api/game/${selectedGame}/move`, { row, col, player: currentPlayer });
+            const updatedGame = res.data;
+            setBoard(updatedGame.board);
+            setCurrentPlayer(updatedGame.currentPlayer);
+            setGameState(updatedGame.state);
+        } catch (error) {
+            console.error("Error making move", error);
+        }
+    };
+
+    const getGame = async (gameId) => {
+        try {
+            const res = await axios.get(`/api/game/${gameId}`);
+            const game = res.data;
+            setSelectedGame(gameId);
+            setBoard(game.board);
+            setCurrentPlayer(game.currentPlayer);
+            setGameState(game.state);
+        } catch (error) {
+            console.error("Error getting game", error);
+        }
+    };
+
+    useEffect(() => {
+        // Optionally, load existing games when the component mounts
+    }, []);
+
+    return (
+        <div>
+            <button onClick={createGame}>Create Game</button>
+            <div>
+                <h2>Games</h2>
+                <ul>
+                    {Object.keys(games).map((gameId) => (
+                        <li key={gameId} onClick={() => getGame(gameId)}>
+                            Game {gameId}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            {selectedGame && (
+                <div>
+                    <h2>Game {selectedGame}</h2>
+                    <div>
+                        {board.map((row, rowIndex) => (
+                            <div key={rowIndex}>
+                                {row.map((cell, colIndex) => (
+                                    <button
+                                        key={colIndex}
+                                        onClick={() => makeMove(rowIndex, colIndex)}
+                                        disabled={cell !== null}
+                                    >
+                                        {cell}
+                                    </button>
+                                ))}
+                            </div>
+                        ))}
+                    </div>
+                    <p>Current Player: {currentPlayer}</p>
+                    <p>Game State: {gameState}</p>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+let games = {};
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
@@ -10,7 +11,66 @@ app.get("/api", (req, res) => {
     res.json({ message: "Mfer Castle!" });
 });
 
-// All other GET requests not handled before will return our React app
+app.use(express.json()); // to parse JSON body
+
+app.post("/api/game", (req, res) => {
+    const id = Date.now().toString();
+    games[id] = {
+        board: [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null]
+        ],
+        currentPlayer: 'X', // X starts the game
+        state: "ongoing", // game status: ongoing, draw, X-wins, O-wins
+    };
+    res.status(201).json({ gameId: id });
+});
+
+app.get("/api/game/:id", (req, res) => {
+    const game = games[req.params.id];
+    if (!game) return res.status(404).json({ message: "Game not found" });
+    res.json(game);
+});
+
+app.post("/api/game/:id/move", (req, res) => {
+    const game = games[req.params.id];
+    if (!game) return res.status(404).json({ message: "Game not found" });
+
+    const { row, col, player } = req.body;
+    if (row == null || col == null || player == null) return res.status(400).json({ message: "Row, column, and player are required" });
+    if (game.board[row][col] !== null) return res.status(400).json({ message: "Cell is already occupied" });
+    if (player !== game.currentPlayer) return res.status(400).json({ message: "Not your turn" });
+    if (game.state !== "ongoing") return res.status(400).json({ message: "Game is already finished" });
+
+    game.board[row][col] = player;
+    game.currentPlayer = player === 'X' ? 'O' : 'X'; // switch player
+
+    // Check game state and update accordingly
+    game.state = checkGameState(game.board, player);
+
+    res.json(game);
+});
+
+function checkGameState(board, player) {
+    // Check rows, columns, and diagonals for a win
+    for (let i = 0; i < 3; i++) {
+        if (board[i].every(cell => cell === player) ||
+            [0, 1, 2].every(col => board[col][i] === player)) {
+            return `${player}-wins`;
+        }
+    }
+    if ([0, 1, 2].every(i => board[i][i] === player) ||
+        [0, 1, 2].every(i => board[i][2 - i] === player)) {
+        return `${player}-wins`;
+    }
+    // Check for a draw
+    if (board.flat().every(cell => cell !== null)) return 'draw';
+
+    return 'ongoing';
+}
+
+
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
