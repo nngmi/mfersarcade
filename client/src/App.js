26e15c576580a41e1f349dbaf414b2d1e73ee28b
nngmi <arewenngmi@gmail.com>
2023-09-26 19:43:41 -0500

    added font and favico

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
@@ -14,7 +13,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="/logo512.png" className="App-logo" alt="logo" />
         <p>{!data ? "Loading..." : data}</p>
       </header>
     </div>
--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+    res.json({ message: "Mfer Castle!" });
 });
 
 // All other GET requests not handled before will return our React app
